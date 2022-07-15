f3bcf9c77c01fb332f3e9fccf81cd1852cd6907c
renansouza95 <s.renan@outlook.com>
2022-07-15 17:29:02 -0300

    fix: ano minimo no schema estava errado

--- src/schemas/CarSchema.ts
+++ src/schemas/CarSchema.ts
@@ -5,13 +5,13 @@ export interface CarDocument extends Car, Document {}
 
 const carSchema = new Schema<Car>({
   model: { type: String, required: true, minlength: 3 },
-  year: { type: Number, required: true, min: 1990, max: 2022 },
+  year: { type: Number, required: true, min: 1900, max: 2022 },
   color: { type: String, required: true, minlength: 3 },
   status: { type: Boolean, required: false },
   buyValue: { type: Number, required: true },
   doorsQty: { type: Number, required: true, min: 2, max: 4 },
   seatsQty: { type: Number, required: true, min: 2, max: 7 },
-});
+}, { versionKey: false });
 
 const carMongooseModel = model<Car>('Cars', carSchema);
 

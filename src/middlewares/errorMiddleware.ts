48481a8f5703275ce84dd11fd182875a44c9229a
renansouza95 <s.renan@outlook.com>
2022-07-15 17:30:00 -0300

    feat: cria middleware de erro

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,22 @@
+import { Error } from 'mongoose';
+import { Request, Response, NextFunction } from 'express';
+import CustomError from './errors/CustomError';
+
+export default function errorMiddleware(
+  error: unknown,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  // se for um erro derivado do custom error eu posso pegar o status code do erro e retornar
+  if (error instanceof CustomError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+  // se for um error de validação do mongoose posso retornar 400 e a mensagem do erro
+  if (error instanceof Error.ValidationError) {
+    return res.status(400).json({ message: error.message });
+  }
+  // se for um erro desconhecido, retorno 500 e mostro no log para acompanhamento da equipe de dev
+  console.log(error);
+  return res.status(500).json({ message: 'internal error' });
+}
